3705a1df64824c2175db6c7cd11142037a2312d7
upqnu <upgrdrqnu@gmail.com>
2023-11-12 22:08:56 +0900

    init commit

---
+++ src/main/java/pp/rsmmm/RsmmmApplication.java
@@ -0,0 +1,13 @@
+package pp.rsmmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsmmmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pp/rsmmm/SecurityConfig.java
@@ -0,0 +1,58 @@
+package pp.rsmmm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+//                .csrf().disable()
+//                .csrf((csrf) -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+//                .cors().disable()
+//                .cors(withDefaults())
+                .cors(Customizer.withDefaults())
+//                .headers(headers -> headers.frameOptions().sameOrigin())
+                .headers((headers) ->
+                        headers
+                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/", "/h2-console/**", "/swagger-ui/**", "/api/consumer/sign-up", "/api/consumer/sign-in"
+                        ).permitAll()
+                        // 권한 설정에 따라 페이지별로 적절한 권한을 부여할 것
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
